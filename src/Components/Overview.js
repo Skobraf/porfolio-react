8c9302b953b7afa7a62c171957ff7b22364f348b
skobraf <skobraf@gmail.com>
2020-01-10 20:14:21 +0000

    display name with efects

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 
 const Overview = () => {
+ const [name, setName] = useState('');
+ const [index, setIndex] = useState(0);
+    useEffect(() => {
+      split();
+      
+    }, [])
+  const nom = [
+    "A",
+    "y",
+    "o",
+    "u",
+    "b",
+    " ",
+    "A",
+    "b",
+    "e",
+    "d"
+    ];
+    let nameRef = useRef(name);
+    let indexRef = useRef(index);
+    indexRef.current = index;
+    nameRef.current = name;
+    const split =  () => {
+      if(nameRef.current.length < nom.length) {
+      setName(nameRef.current + nom[indexRef.current]); 
+      setIndex(indexRef.current + 1); 
+      setTimeout(split, 400);
+    }
+    return;
+    }
 
     return (
         <div className="overview">
           <div className="title">
             <h1 id="typing" className="yes"></h1>
-            <h1 className="not"><span>&nbsp;</span></h1>
+            <h1 className="not">{name}<span>&nbsp;</span></h1>
           </div>
           <div className="description">
             <p>I am a Front-End Developper. The goal for all my projects is to produce simple, beautiful user experiences. In my prior life, I worked as a Product Manager at iSmartAlarm. Follow my progress by exploring my recent projects below.</p>
